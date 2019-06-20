bb7091940fc300913a2217cff7f151afe0e8c0fd
Phong Ho <htphong24@gmail.com>
2019-06-20 16:34:28 +0700

    Rename Controller and remove unnecessary code

---
+++ AspnetCoreSPA/Controllers/ContactController.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspnetCoreSPATemplate.Controllers
+{
+    //[Route("api/[controller]")]
+    public class ContactController : Controller
+    {
+        
+    }
+}
