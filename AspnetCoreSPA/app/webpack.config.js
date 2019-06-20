de3835136a6d949d296a76459d075c1a05f0dd9a
Phong Ho <htphong24@gmail.com>
2019-06-20 16:45:46 +0700

    add style-loader

--- AspnetCoreSPA/app/webpack.config.js
+++ AspnetCoreSPA/app/webpack.config.js
@@ -24,4 +24,33 @@ module.exports = {
       template: 'index.html',
     }),
   ],
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      }
+    ]
+  }
+
 };
