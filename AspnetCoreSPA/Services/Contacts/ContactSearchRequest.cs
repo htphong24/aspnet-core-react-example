a176e673d4f475b360e563b14cfe0035854a5a48
Phong Ho <htphong24@gmail.com>
2020-01-14 15:18:08 -0800

    Refactored namespace - Contacts (cont)

---
+++ AspnetCoreSPA/Services/Contacts/ContactSearchRequest.cs
@@ -0,0 +1,13 @@
+﻿using AspnetCoreSPATemplate.Services.Common;
+
+namespace AspnetCoreSPATemplate.Services.Contacts
+{
+    public class ContactSearchRequest : SearchRequest
+    {
+        public ContactSearchRequest()
+            : base()
+        {
+
+        }
+    }
+}
